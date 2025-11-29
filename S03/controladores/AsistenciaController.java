5ce5ba8cb8ea22dfe74cf76f8c5ec546093560a6
bibnat <86243936+bibnat@users.noreply.github.com>
2025-11-29 20:17:56 -0300

    Refactor AsistenciaController to return entities directly

--- S03/controladores/AsistenciaController.java
+++ S03/controladores/AsistenciaController.java
@@ -1,12 +1,7 @@
 package tuti.daos.presentacion;
 
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
-
 import tuti.daos.entidades.Asistencia;
-import tuti.daos.entidades.Racion;
-import tuti.daos.entidades.Asistido;
 import tuti.daos.presentacion.dto.AsistenciaDTO;
 import tuti.daos.servicios.AsistenciaService;
 
@@ -28,53 +23,18 @@ public class AsistenciaController {
     }
 
     @GetMapping("/{id}")
-    public EntityModel<Asistencia> obtener(@PathVariable Integer id) {
-
-        Asistencia a = service.buscarPorId(id);
-
-        Asistido asistido = a.getAsistido();
-        Racion racion = a.getRacion();
-
-        return EntityModel.of(
-                a,
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(AsistenciaController.class).obtener(id)
-                ).withSelfRel(),
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(AsistidoController.class)
-                                .obtener(asistido.getId())
-                ).withRel("asistido"),
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(RacionController.class)
-                                .obtener(racion.getId())
-                ).withRel("racion"),
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(RecetaController.class)
-                                .obtener(racion.getReceta().getId())
-                ).withRel("receta"),
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(AsistenciaController.class).listar()
-                ).withRel("asistencias")
-        );
+    public Asistencia obtener(@PathVariable Integer id) {
+        return service.buscarPorId(id);
     }
 
-    @GetMapping("/por-asistido/{idAsistido}")
+    @GetMapping("/asistido/{idAsistido}")
     public List<Asistencia> listarPorAsistido(@PathVariable Integer idAsistido) {
         return service.listarPorAsistido(idAsistido);
     }
 
     @PostMapping
-    public EntityModel<Asistencia> crear(@RequestBody AsistenciaDTO dto) {
-
-        Asistencia creada = service.crearDesdeDTO(dto);
-
-        return EntityModel.of(
-                creada,
-                WebMvcLinkBuilder.linkTo(
-                        WebMvcLinkBuilder.methodOn(AsistenciaController.class)
-                                .obtener(creada.getId())
-                ).withSelfRel()
-        );
+    public Asistencia crear(@RequestBody AsistenciaDTO dto) {
+        return service.crearDesdeDTO(dto);
     }
 
     @DeleteMapping("/{id}")
