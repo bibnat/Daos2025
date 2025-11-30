702bb562d77397cd43c0a8467530a8156978eb46
GuilleCiber01 <41122035+guillermomendez@users.noreply.github.com>
2025-11-30 17:06:16 -0300

    Add files via upload

---
+++ S02/RacionDTO.java
@@ -0,0 +1,10 @@
+package tuti.daos.presentacion.dto;
+
+public class RacionDTO {
+
+    public Integer idReceta;
+    public Integer stockPreparado;
+    public String fechaPreparacion;
+    public String fechaVencimiento;
+
+}
